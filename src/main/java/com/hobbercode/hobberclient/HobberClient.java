47d11f429d37495e66b285912e99f17c7cf47d7b
Owen Hobbs <owenhobbs2006@gmail.com>
2021-03-10 21:04:43 -0600

    Add side verification

--- src/main/java/com/hobbercode/hobberclient/HobberClient.java
+++ src/main/java/com/hobbercode/hobberclient/HobberClient.java
@@ -3,16 +3,21 @@ package com.hobbercode.hobberclient;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = HobberClient.MODID, version = HobberClient.VERSION)
 public class HobberClient {
 	
     public static final String MODID = "hobberclient";
-    public static final String NAME = "HobberClient";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
+    	if(event.getSide() == Side.SERVER) {
+    		System.out.println("HobberClient isn't compatable on the server!");
+    		return;
+    	}
+    	
         System.out.println("HobberClient initialized");
     }
     
